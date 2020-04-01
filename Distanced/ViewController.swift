cf0015b26c5678d28c89eaae8f0beda17db3c01e
Max <maxzheleznyy@gmail.com>
2020-04-01 09:50:52 -0700

    Initial Commit

---
+++ Distanced/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Distanced
+//
+//  Created by Maxim Zheleznyy on 4/1/20.
+//  Copyright © 2020 Maxim Zheleznyy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
